939c73c0d76fefd262599e4a512f0aa43c835e6b
Mats de Waard <mats.de.waard@gmail.com>
2024-06-28 17:06:01 +0200

    Init template files

--- services/flask/project/config.py
+++ services/flask/project/config.py
@@ -1,5 +1,6 @@
 import os
-
+import random
+import string
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -7,23 +8,29 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 def read_secret_file(file_path: str) -> str:
     """Reads the content of a secret file and returns it as a string."""
     try:
-        with open(file_path, 'r') as file:
+        with open(file_path, "r") as file:
             return file.read().strip()
     except FileNotFoundError:
         raise ValueError(f"Secret file {file_path} not found")
 
-class Config(object):
 
+class Config(object):
     @staticmethod
     def get_database_uri() -> str:
         required_env_vars = ["SQL_HOST", "SQL_PORT"]
-        required_secrets = ["POSTGRES_USER_FILE", "POSTGRES_PASSWORD_FILE", "POSTGRES_DB_FILE"]
+        required_secrets = [
+            "POSTGRES_USER_FILE",
+            "POSTGRES_PASSWORD_FILE",
+            "POSTGRES_DB_FILE",
+        ]
 
         is_debug = os.getenv("FLASK_DEBUG") == "1"
 
         missing_vars = [var for var in required_env_vars if not os.getenv(var)]
         if missing_vars:
-            raise ValueError(f"One or more required environment variables are missing: {', '.join(missing_vars)}")
+            raise ValueError(
+                f"One or more required environment variables are missing: {', '.join(missing_vars)}"
+            )
 
         host = os.getenv("SQL_HOST")
         port = os.getenv("SQL_PORT")
@@ -33,11 +40,15 @@ class Config(object):
             password = os.getenv("POSTGRES_PASSWORD")
             db_name = os.getenv("POSTGRES_DB")
             if not all([user, password, db_name]):
-                raise ValueError("One or more required environment variables for database credentials are missing")
+                raise ValueError(
+                    "One or more required environment variables for database credentials are missing"
+                )
         else:
             missing_secrets = [var for var in required_secrets if not os.getenv(var)]
             if missing_secrets:
-                raise ValueError(f"One or more required secret files are missing: {', '.join(missing_secrets)}")
+                raise ValueError(
+                    f"One or more required secret files are missing: {', '.join(missing_secrets)}"
+                )
 
             user_file = os.getenv("POSTGRES_USER_FILE")
             password_file = os.getenv("POSTGRES_PASSWORD_FILE")
@@ -53,3 +64,6 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ASSETS_ROOT = os.getenv("ASSETS_ROOT", "/static")
 
+    SECRET_KEY = os.getenv("SECRET_KEY", None)
+    if not SECRET_KEY:
+        SECRET_KEY = "".join(random.choice(string.ascii_lowercase) for i in range(32))
